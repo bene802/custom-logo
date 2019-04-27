2b342a1f713427c10e5d29eab3e5fd51cb836a23
bene802 <wangzetong94@gmail.com>
2019-04-27 15:55:43 -0400

    image uploaded

--- my-app/src/components/Setting.jsx
+++ my-app/src/components/Setting.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import ImageUpload from "./ImageUpload";
 
 class Setting extends Component {
-  state = {};
   render() {
-    return <div>Setting</div>;
+    const url =
+      "https://s3.amazonaws.com/images.clearviewsocial/CVSLogo.FullColor.RGB+(2).png";
+    return (
+      <div>
+        Setting
+        <button onClick={() => this.props.getImageInfo()}>click</button>
+        <div>
+          <ImageUpload />
+        </div>
+      </div>
+    );
   }
 }
 
